7dd9c2d66ced2b8eed4659333b79762a02a69c68
libertymaxi <libertymaxi912@protonmail.com>
2024-07-27 00:19:26 +0100

    Added a new Vector class with basic operations

---
+++ src/vector.cpp
@@ -0,0 +1,73 @@
+#include "vector.h"
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+Vector :: Vector(vector<double> newElements) {
+    elements = newElements;
+}
+
+Vector :: Vector(unsigned nElements) {
+    for(int i = 0; i < nElements; i++) elements.push_back(0.0);
+}
+
+void Vector :: printVector(void) {
+    cout << "[";
+    for(int i = 0; i < elements.size(); i++) {
+        if(elements[i] < 0) cout << "-";
+        else cout << "+";
+        cout << setprecision(3) << fixed << fabs(elements[i]);
+        if(i < elements.size() - 1) cout << " ";
+    }
+    cout << "]" << endl;
+}
+
+double Vector :: operator[](unsigned index) {
+    return elements[index];
+}
+
+Vector Vector :: operator+(Vector vectorOperator) {
+    vector<double> newElements;
+    if(elements.size() < vectorOperator.elements.size()) {
+        for(int i = 0; i < elements.size(); i++) {
+            newElements.push_back(elements[i] + vectorOperator.elements[i]);
+        }
+        for(int j = elements.size(); j < vectorOperator.elements.size(); j++) {
+            newElements.push_back(vectorOperator.elements[j]);
+        }
+    }
+    else {
+        for(int i = 0; i < vectorOperator.elements.size(); i++) {
+            newElements.push_back(elements[i] + vectorOperator.elements[i]);
+        }
+        for(int j = vectorOperator.elements.size(); j < elements.size(); j++) {
+            newElements.push_back(elements[j]);
+        }
+    }
+    return Vector(newElements);
+}
+
+void Vector :: operator+=(Vector vectorOperator) {
+    elements = operator+(vectorOperator).elements;
+}
+
+Vector Vector :: operator*(double number) {
+    vector<double> newElements;
+    for(int i = 0; i < elements.size(); i++) {
+        newElements.push_back(elements[i] * number);
+    }
+    return Vector(newElements);
+}
+
+void Vector :: operator*=(double number) {
+    elements = Vector(operator*(number)).elements;
+}
+
+Vector Vector :: operator-(Vector vectorOperator) {
+    return operator+(vectorOperator * -1);
+}
+
+void Vector :: operator-=(Vector vectorOperator) {
+    elements = Vector(operator-(vectorOperator)).elements;
+}
---
+++ src/vector.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <vector>
+
+using namespace std;
+
+class Vector {
+    private:
+        vector<double> elements;
+    public:
+        Vector(vector<double>);
+        Vector(unsigned);
+        void printVector(void);
+        double operator[](unsigned);
+        Vector operator+(Vector);
+        void operator+=(Vector);
+        Vector operator*(double);
+        void operator*=(double);
+        Vector operator-(Vector);
+        void operator-=(Vector);
+};
+
+#endif
